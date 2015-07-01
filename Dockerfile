84d12451276a40b50da9f25e57c988b0bf4d343e
VeerMuchandi <veer.muchandi@gmail.com>
2015-07-01 00:43:52 -0400

    First commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM php:5.6-apache
+MAINTAINER Veer Muchandi veer@redhat.com
+COPY src/ /var/www/html/
+Expose 80
---
+++ src/time.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('America/New_York');
+// Print the date and time
+echo date('l jS \of F Y h:i:s A');
+?>
