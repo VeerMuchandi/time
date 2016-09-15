39a16e6b59ad96b0a15714e54d6eaebbaa1fb164
VeerMuchandi <veer.muchandi@gmail.com>
2016-09-14 20:45:02 -0400

    Update init.sh

--- rhel/init.sh
+++ rhel/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)\nHost: $(hostname)" | nc -ll -p 8080; done
+while true; do echo -e "HTTP/1.1 200 OK\n\n$(date)\nHost: $(tail -1 /etc/hosts| awk '{print $2}')" | nc -ll -p 8080; done
 
